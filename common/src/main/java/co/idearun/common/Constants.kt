678a884615fb3fa48a9d329423f8488b0e358319
Elham Feyzolahi <ellyfeyzolahi91@gmail.com>
2021-08-23 15:34:38 +0430

    remove extra files

--- common/src/main/java/co/idearun/common/Constants.kt
+++ common/src/main/java/co/idearun/common/Constants.kt
@@ -1,14 +1,11 @@
 package co.idearun.common
 
-import android.Manifest
 
 object Constants {
 
     const val AUTO_NEXT_DELAY: Long = 400
     const val SCROLL_DELAY: Long = 150
 
-    val PERMISSIONS_EXTERNAL_STORAGE = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    const val PERMISSION_ALL = 1
     const val FORM = "form"
     const val DROPDOWN = "dropdown"
     const val MATRIX = "matrix"
@@ -31,27 +28,10 @@ object Constants {
     const val SECTION = "section"
     const val PHONE_VERIFICATION = "phone_verification"
     const val Like_Dislike = "like_dislike"
-
-    const val mobile = "mobile"
-    const val landline = "landline"
-    const val both = "both"
-
     const val image = "image"
-    const val document = "document"
-    const val all = "all"
-
-    const val irr = "irr"
-    const val irt = "irt"
-    const val usd = "usd"
-    const val eur = "eur"
-    const val other = "other"
-
     const val embeded = "embeded"
     const val star = "star"
-    const val like_dislike = "like dislike"
     const val nps = "nps"
-    const val score = "score"
-
     const val PREFERENCES_PROGRESS = "PREFERENCES_PROGRESS"
     const val PREFERENCES_LAST_Lesson = "PREFERENCES_LAST_FORM"
 
