e199a3b9fa7772157f42918976602ff899c5397c
Elham Feyzolahi <ellyfeyzolahi91@gmail.com>
2021-08-23 13:59:22 +0430

    update about page

--- feature/src/main/java/co/idearun/feature/AboutActivity.kt
+++ feature/src/main/java/co/idearun/feature/AboutActivity.kt
@@ -1,17 +1,13 @@
 package co.idearun.feature
 
-import android.os.Build
 import android.os.Bundle
-import android.text.Html
-import android.text.method.LinkMovementMethod
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import co.idearun.feature.databinding.ActivityAboutBinding
-import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityAboutBinding
+    private lateinit var binding: ActivityAboutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_about)
