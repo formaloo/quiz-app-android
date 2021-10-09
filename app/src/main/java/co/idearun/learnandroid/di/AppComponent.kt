4b7dae3ca44d39cfaa2299507a0529de83a2eed9
mahdi <mahdidrv78@gmail.com>
2021-10-09 12:12:04 +0330

    create game module

--- app/src/main/java/co/idearun/learnandroid/di/AppComponent.kt
+++ app/src/main/java/co/idearun/learnandroid/di/AppComponent.kt
@@ -1,15 +1,17 @@
 package co.idearun.learnandroid.di
 
-import co.idearun.common.BuildConfig.BASE_URL
-import co.idearun.common.BuildConfig.X_API_KEY
 import co.idearun.data.local.di.formBuilderLocalModule
 import co.idearun.data.remote.di.createRemoteFormModule
 import co.idearun.data.repository.di.formRepositoryModule
 import co.idearun.feature.di.featureModule
+import co.idearun.game.di.gameAppModule
+import co.idearun.learnandroid.BuildConfig.BASE_URL
+import co.idearun.learnandroid.BuildConfig.X_API_KEY
 
 val appComponent = listOf(
     createRemoteFormModule(BASE_URL, X_API_KEY),
     formBuilderLocalModule,
     formRepositoryModule,
-    featureModule
+    featureModule,
+    gameAppModule
 )
